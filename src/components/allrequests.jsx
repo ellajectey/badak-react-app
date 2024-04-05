0457121c5ec64c40bed7c1ccd49f6b4951c5af6e
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-04-05 14:01:38 +0000

    added new components and pages

---
+++ src/components/allrequests.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Allrequests() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Allrequests;
---
+++ src/components/registercard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Registercard() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Registercard;
