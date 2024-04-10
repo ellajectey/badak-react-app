c4e2217a7b1869d72953d5a1970bdcb5f5c0dca1
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-04-10 09:39:58 +0000

    authentication for login done using local storage (formik and yap)

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import "../index.css";
 import img from "../assets/badak-logo.png";
+// import {useNavigate} from react-router-Dom;
 
 function Sidebar() {
+
+// const Navigate = useNavigate()
+// const Logout= () =>{
+//   window.localStorage.removeItem("isLoggedIn")
+//   Navigate('/')
+// }
   return (
     <div>
       {/* Sidebar */}
       <aside className="flex min-h-screen shadow bg-white border-2 border-blue-600 pt-20 pb-10 overflow-x-hidden">
         <div>
           {/* Menus */}
-          <img src={img} alt="badak logo" className="w-60 h-60 text-center" />
+          <img src={img} alt="badak logo" className="w-40 h-15 text-center" />
           <ul className="w-auto p-10">
             <li>
               <label>Menus</label>
               <hr />
             </li>
-            <li className="list-none flex items-center p-8 hover:list-none hover:flex hover:items-center hover:p-1 hover:bg-blue-300 hover:rounded-md">
+            <li className="list-none flex items-center p-8 hover:list-none hover:flex hover:p-1 hover:bg-blue-300 hover:rounded-md">
               <i className="fas fa-sliders-h  w-20 text-blue-600 px-4 text-center"></i>
               <h5>Dashboard</h5>
             </li>
@@ -40,8 +47,9 @@ function Sidebar() {
               <h5>Infos</h5>
             </li>
             <li className="list-none flex items-center p-8 mt-40 hover:list-none hover:flex hover:items-center hover:p-1 hover:bg-blue-300 hover:rounded-md">
+            <button>
               <i className="fas fa-sign-out-alt  w-20 text-blue-600 px-4 text-center "></i>
-              <h5>Se deconnecter</h5>
+              <h5>Se deconnecter</h5></button>
             </li>
           </ul>
         </div>
